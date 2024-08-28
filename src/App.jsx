3f44d0ddcff43465490f0e547998f8eb2758750e
NaufalExpInc <naufal@expinc.io>
2024-08-28 14:59:16 +0700

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-                      </div>
+                            </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count} {name}
-        </button>
+                  </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
